19bc47f667b4ca78d5ad01297fe13fa65a95a676
祥平 佐藤 <150115@jc-21.jp>
2018-01-10 12:55:53 +0900

    fgh

---
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/Training.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+    public class Training
+    {
+
+        public string Menu { get; set; }
+        public string Load { get; set; }
+        public string parts { get; set; }
+
+        public string getMenu()
+        {
+            return Menu;
+        }
+
+
+        public override string ToString()
+        {
+            return Menu + "                                                                                負荷度：" + Load;
+        }
+
+    }
+}
