b7af649ce04a8dc2ed6b660bb0e50fd959748766
祥平 佐藤 <150115@jc-21.jp>
2018-01-11 09:27:20 +0900

    gji

---
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/MenudetaliPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MuscleTrainingRecords00
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenudetaliPage : ContentPage
+    {
+        public MenudetaliPage(String l)
+        {
+            InitializeComponent();
+
+
+            Transition.Text = l;
+        }
+
+        /*public MenudetaliPage(String l)
+        {
+        this.l = l;
+        }*/
+    }
+}
